a3f73446ee105f73f3a03b50dc2dc4abe751aeea
Scott Hansen <scottiehansen@gmail.com>
2021-06-04 17:05:40 -0700

    Deleted keys

--- server/controllers.js
+++ server/controllers.js
@@ -5,8 +5,6 @@ const key = require('./config/config.js')
 const controllers = {
   getProducts: (req, res) => {
     const config = {
-      // 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
-      // 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
       headers: {Authorization: `${key}`}
     };
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products`, config)
