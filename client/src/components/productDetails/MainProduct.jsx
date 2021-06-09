6a9797757f39b71704f01379180b46101e4df044
Austin Yeon <austinyeon@gmail.com>
2021-06-09 13:06:14 -0700

    fixed issue with too many API requests and created style component

--- client/src/components/productDetails/MainProduct.jsx
+++ client/src/components/productDetails/MainProduct.jsx
@@ -3,23 +3,29 @@ import axios from 'axios';
 import ProductFeatures from './ProductFeatures.jsx';
 const key = require('/client/src/config/config.js');
 import ProductImages from './ProductImages.jsx';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 function MainProduct (props) {
   const [item, setItem] = useState({});
   const [features, setFeatures] = useState([]);
   const [images, setImages] = useState('');
+  const [availableStyles, setAvailableStyles] = useState([]);
 
-  useEffect(() => {
+  useEffect(async () => {
     setItem(props.item);
     const config = {
       headers: {Authorization: `${key.API_KEY}`}
     };
-    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}`, config)
-      .then(response => {
-        setFeatures(response.data.features);
-      })
-      .catch(error => {console.log(error)})
-  })
+    const [featureResponse, imageStyleResponse] = await Promise.all([
+      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}`, config),
+      axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}/styles`, config)
+    ]);
+    setFeatures(featureResponse.data.features);
+    console.log(imageStyleResponse.data);
+    setImages(imageStyleResponse.data);
+    setAvailableStyles(imageStyleResponse.data.results);
+  }, [])
 
   return (
     <div>
@@ -36,3 +42,28 @@ function MainProduct (props) {
 export default MainProduct;
 
 // {item.features.map((feature, index) => <ProductFeatures feature={feature} key={index}/> )}
+
+//SIMPLE AXIOS REQUEST
+// axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}`, config)
+//       .then(response => {
+//         setFeatures(response.data.features);
+//       })
+//       .catch(error => {console.log(error)})
+
+//AXIOS PROMISE CHAINING
+// const [featureResponse, imageResponse] = await Promise.all([
+//   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}`, config),
+//   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}/styles`, config)
+// ]);
+
+//AXIOS GET CHAINING
+// axios
+//       .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}`, config)
+//       .then(response => {
+//         setFeatures(response.data.features);
+//         return axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${props.item.id}/styles`, config)
+//       })
+//       .then(response => {
+//         setImages(response.data);
+//       })
+//       .catch(error => console.log(error))
