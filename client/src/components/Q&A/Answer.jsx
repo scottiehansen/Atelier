529872f50d4c39b7ece103c8518668ec42d43015
Raelyn Cheung-Sutton <rcheungsutton@gmail.com>
2021-06-09 15:57:56 -0700

    converted ISO timestamp to Month, dd, yyyy format

--- client/src/components/Q&A/Answer.jsx
+++ client/src/components/Q&A/Answer.jsx
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from 'react'
 
 function Answer(props) {
 
+  //convert ISO time stamp to time obj and then to useable format
+  var dateStr = props.answer.date;
+  var dateObj = new Date(dateStr);
+  var longEnUSFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  var formattedDate = longEnUSFormatter.format(dateObj)
+
   return (
     <li>
       <p>{props.answer.body}</p>
       <span>
-        by {props.answer.answerer_name}, {props.answer.date} | Helpful? <button className="link-button">Yes</button> ({props.answer.helpfulness}) | <button className="link-button">Report</button>
+        by {props.answer.answerer_name}, {formattedDate} | Helpful? <button className="link-button">Yes</button> ({props.answer.helpfulness}) | <button className="link-button">Report</button>
       </span>
     </li>
   )
