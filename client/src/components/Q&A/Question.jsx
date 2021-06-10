8f65c8afce35292a7691952588a8766f043ece6e
Raelyn Cheung-Sutton <rcheungsutton@gmail.com>
2021-06-09 21:04:46 -0700

    able to update question's helpfulness score via put request

--- client/src/components/Q&A/Question.jsx
+++ client/src/components/Q&A/Question.jsx
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import Answer from './Answer.jsx'
+import token from '../../../../server/config/config.js'
+import axios from 'axios'
+
+// repeated code? will have to repeat this if i make a request from any component file?
+const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
+const auth = {
+  headers: {
+    Authorization: token.API_KEY
+  }
+};
 
 function Question(props) {
 
   const [answersLimit, setAnswersLimit] = useState(2);
+  const [markedHelpful, setMarkedHelpful] = useState(false);
 
   //sorts by seller's answers and then by helpfulness
   const answersArr = Object.values(props.question.answers).sort(function(a, b) {
@@ -18,7 +29,7 @@ function Question(props) {
     }
   });
 
-  // renders desired number of answers on page with buttons to adjust the numbers
+  // renders desired number of answers on page with button to render more or less answers
   var moreAnswers;
   if(answersArr.length <= 2){
     moreAnswers = null;
@@ -28,6 +39,28 @@ function Question(props) {
     moreAnswers = <button onClick={() => (setAnswersLimit(answersArr.length))}>See more answers</button>
   }
 
+  //udpate Q's helpfulness rating if the Q has not yet been marked helpful
+  function updateHelpfulness(id, currentHelpfulness){
+    console.log('before', markedHelpful)
+    if(!markedHelpful){
+      var newHelpfulness = {
+        helpfulness: currentHelpfulness + 1
+      };
+      axios.put(`${url}/qa/questions/${id}/helpful`, newHelpfulness, auth)
+        .then((response) => {
+          setMarkedHelpful(!markedHelpful);
+          console.log('then block', markedHelpful)
+          // re-render
+        })
+        .then((response) =>{
+          console.log('extra then', markedHelpful)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+  }
+
 
   return (
     <li className="QA">
@@ -35,7 +68,10 @@ function Question(props) {
         <p>Q: {props.question.question_body}</p>
         <span>
           Helpful?
-          <button className="link-button">Yes</button>
+          <button
+            className="link-button"
+            onClick={()=>{updateHelpfulness(props.id, props.question.question_helpfulness)}}
+          >Yes</button>
           ({props.question.question_helpfulness}) |
           <button className="link-button">Add Answer</button>
         </span>
