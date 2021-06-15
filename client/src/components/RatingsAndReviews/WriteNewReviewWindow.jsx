d9da94d8ea0fd3252005a141281c97951bdceb39
Scott Hansen <scottiehansen@gmail.com>
2021-06-15 11:57:01 -0700

    Added body and summary components for write new reviews. Functionality is currently working, the CSS is still unfinished.

--- client/src/components/RatingsAndReviews/WriteNewReviewWindow.jsx
+++ client/src/components/RatingsAndReviews/WriteNewReviewWindow.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {useState, useReducer} from 'react';
 import Recommendation from './WriteNewReviewWindowComponents/Recommendation.jsx'
 import Characteristics from './WriteNewReviewWindowComponents/Characteristics.jsx'
+import Summary from './WriteNewReviewWindowComponents/Summary.jsx'
 
 // YOU NEED TO PASS THE PRODUCT NAME DOWN AS A PROP TO THIS FUNCTION FROM THE APP FUNCTION
 
 const WriteNewReviewWindow = (props) => {
   let [productRecommendation, changeProductRecommendation] = useState(null) // or no value
-  let [productCharacteristics, makeProductCharacteristics] = useState({Size: null, Width: null, Comfort: null, Quality: null, Length: null, productFit: null})
+  let [productCharacteristics, makeProductCharacteristics] = useState({Size: null, Width: null, Comfort: null, Quality: null, Length: null, Fit: null})
+  let [reviewSummary, makeReviewSummary] = useState('')
+  let [reviewBody, makeReviewBody] = useState('')
 
   if (props.showWindow) {
     return (
@@ -18,6 +21,7 @@ const WriteNewReviewWindow = (props) => {
           <div>Placeholder for stars component </div>
           <Recommendation productRecommendation={productRecommendation} changeProductRecommendation = {changeProductRecommendation} />
           <Characteristics productCharacteristics={productCharacteristics} makeProductCharacteristics = {makeProductCharacteristics} />
+          <Summary reviewSummary = {reviewSummary} reviewBody = {reviewBody} makeReviewSummary = {makeReviewSummary} makeReviewBody={makeReviewBody} />
           <label>
             PLACEHOLDER LABEL:
             <textarea/>
---
+++ client/src/components/RatingsAndReviews/WriteNewReviewWindowComponents/Photo.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+
+// const Photos = (props) => {
+//   return (
+//     <input type="file" onChange={this.fileChangedHandler}>
+//     <button onClick={this.uploadHandler}>Upload!</button>
+//   )
+// }
