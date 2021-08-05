fd1489beafc01bc749adda80c701dfa77b71bcc5
Austin Yeon <austinyeon@gmail.com>
2021-08-04 18:10:02 -0700

    fixed issue with centering of modal image window

--- client/src/components/productDetails/modals/ImageModal.jsx
+++ client/src/components/productDetails/modals/ImageModal.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
+import ModalDialog from 'react-bootstrap/ModalDialog'
 
 export default function ImageModal (props) {
   if (!props.showImageModal) {
     return null;
   }
   return (
-    <Modal show={props.showImageModal} onHide={props.show}>
-      <img src={props.images[props.mainImageIndex].url}/>
+    <Modal
+      show={props.showImageModal}
+      onHide={props.show}
+      size='xl'
+    >
+      <img className="modal-image" src={props.images[props.mainImageIndex].url}/>
     </Modal>
   )
 }
